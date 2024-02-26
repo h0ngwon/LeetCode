5f5bf0a30cf9b342585f7f34f8a0654e36e3a624
Hongwon Lim <w___on@naver.com>
2024-02-26 18:06:28 +0900

    Time: 40 ms (98.43%), Space: 49.3 MB (32.93%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -6,6 +6,7 @@
 var gcdOfStrings = function(str1, str2) {
     if(str1 + str2 != str2 + str1) return "";
     
+    //최대 공약수를 찾아서 그 길이만큼 자르기
     const gcd = (a, b) => {
         if(a % b === 0) return b;
         return gcd(b, a % b);
