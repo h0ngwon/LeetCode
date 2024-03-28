928611d5fd5f826333022b4e44799701f64d255a
Hongwon Lim <w___on@naver.com>
2024-03-28 21:50:30 +0900

    Time: 1137 ms (5.01%), Space: 67.9 MB (5.18%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var longestSubarray = function(nums) {
-    // 처음부터 시작해서 0이 2개 등장하면 인덱스 감소 후 계산
     let cur = 0, prev = 0, max = -Infinity;
     
     for(let i = 0; i < nums.length; i++) {
@@ -11,10 +10,9 @@ var longestSubarray = function(nums) {
         else {
             prev = cur;
             cur = 0;
-            //max = Math.max(max, prev + cur);
         }
-        
         max = Math.max(max, prev + cur);
+        console.log(i, max, prev)
     }
     
     return max === nums.length ? max - 1 : max;
