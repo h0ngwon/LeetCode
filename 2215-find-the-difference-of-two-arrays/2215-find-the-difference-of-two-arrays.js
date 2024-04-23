fc6b729482d9abe7a518d5a5179772f4000277eb
Hongwon Lim <w___on@naver.com>
2024-04-23 22:29:14 +0900

    Time: 111 ms (28.03%), Space: 56 MB (60.87%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,23 +4,8 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    let result = [];
-    let arr1 = [], arr2 = [];
-    
-    nums1.map((x) => {
-        if(!nums2.includes(x) && !arr1.includes(x)) {
-            arr1.push(x);
-        }
-    });
-    
-    nums2.map((x) => {
-        if(!nums1.includes(x) && !arr2.includes(x)) {
-            arr2.push(x);
-        }
-    });
-    
-    result.push(arr1);
-    result.push(arr2);
-    
-    return result;
+    return [
+        [...new Set(nums1)].filter((e) => !nums2.includes(e)),
+        [...new Set(nums2)].filter((e) => !nums1.includes(e)),
+    ];
 };
